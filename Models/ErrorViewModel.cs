ceb4a8970fa5389b95a4a98b3c74127832fff49b
FLEN2360 <albert221318@gmail.com>
2025-11-25 12:19:14 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClubActivitiesSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
