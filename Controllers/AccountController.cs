f4933eb602d5e8f55338298686936606a5f9841d
YinCheng0106 <eason010630@gmail.com>
2025-12-09 15:11:23 +0800

    新增登入功能，包括登入控制器、視圖和模型，並更新註冊視圖以支持多語言顯示

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -10,9 +10,24 @@ namespace ClubActivitiesSystem.Controllers
     {
       return View();
     }
+    public IActionResult Login()
+    {
+      return View();
+    }
 
     [HttpPost]
     public IActionResult Register(RegisterViewModel model)
+    {
+      if (ModelState.IsValid)
+      {
+        return RedirectToAction("Index", "Home");
+      }
+
+      return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult Login(LoginViewModel model)
     {
       if (ModelState.IsValid)
       {
--- Models/LoginViewModel.cs
+++ Models/LoginViewModel.cs
@@ -1,16 +1,14 @@
 namespace ClubActivitiesSystem.Models
 {
   using System.ComponentModel.DataAnnotations;
-  public class RegisterViewModel
+  public class LoginViewModel
   {
-    [Required(ErrorMessage = "Email 不為空")]
+    [Required(ErrorMessage = "Email不為空")]
     [EmailAddress]
     public required string Email { get; set; }
     [Required(ErrorMessage = "密碼不為空")]
     [DataType(DataType.Password)]
     public required string Password { get; set; }
-    [Compare("Password", ErrorMessage = "密碼不匹配")]
-    public required string ConfirmPassword { get; set; }
+    public bool RememberMe { get; set; }
   }
 }
-
